1f66549fb391b60a9981815ee2c5cfb80681879c
Denzell Ford <fdenzell@gmail.com>
2020-12-21 00:00:51 -0600

    Added percetage bar

--- progressbar/lib/main.dart
+++ progressbar/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'package:intl/intl.dart';
 
 void main() {
@@ -45,10 +46,64 @@ class _ProgressBarState extends State<ProgressBar>
   AnimationController _controller;
   Animation progressAnimation;
   Animation sizeAnimation;
+  Animation dyingAnimation;
+  Animation bouncingAnimation;
 
-  final formatter = new NumberFormat("###.00");
-
+  final formatter = new NumberFormat("###");
   String get percent => formatter.format(progressAnimation.value * 100);
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+
+    sizeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.0,
+          0.2,
+          curve: Curves.bounceInOut,
+        )));
+
+    progressAnimation = Tween<double>(
+      begin: 0.0,
+      end: 0.5,
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.4,
+          0.8,
+          curve: Curves.easeOut,
+        )));
+
+    bouncingAnimation = Tween<Offset>(
+      begin: Offset(-10, 20),
+      end: Offset(0, 0),
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.4,
+          0.8,
+          curve: Curves.bounceInOut,
+        )));
+    
+    dyingAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+        parent: _controller,
+        curve: Interval(
+          0.8,
+          1.0,
+          curve: Curves.easeOut,
+        )));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,6 +113,7 @@ class _ProgressBarState extends State<ProgressBar>
           return Stack(
             children: [
               Align(
+                alignment: Alignment.center,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20),
                   child: Container(
@@ -77,75 +133,25 @@ class _ProgressBarState extends State<ProgressBar>
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(20),
                   child: Opacity(
-                    opacity: sizeAnimation.value > 0.5 ? sizeAnimation.value : 0,
+                    opacity:
+                        sizeAnimation.value > 0.5 ? sizeAnimation.value : 0,
                     child: Container(
                       width: 350.0 * progressAnimation.value,
-                      height: widget.height,
+                      height: (1 - dyingAnimation.value) * widget.height,
                       color: Colors.white,
                     ),
                   ),
                 ),
               ),
-              Align(
-                child: Opacity(
+              PercentageLabel(
                   opacity: sizeAnimation.value > 0.5 ? sizeAnimation.value : 0,
-                  child: Transform.translate(
-                    offset: Offset(0, -35),
-                    child: Container(
-                      height: widget.height * 4,
-                      width: 140,
-                      color: Colors.white,
-                      child: Center(
-                        child: Text("$percent%",
-                            style: Theme.of(context).textTheme.bodyText1.copyWith(
-                                  color: Colors.black,
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.w400,
-                                )),
-                      ),
-                    ),
-                  ),
-                ),
+                  offset: bouncingAnimation.value,
+                  height: widget.height,
+                  percent: percent,
               ),
             ],
           );
         });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 2),
-    );
-
-    sizeAnimation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Interval(
-           0.0, 0.2,
-           curve: Curves.bounceInOut,
-        )
-      )
-    );
-
-    progressAnimation = Tween<double>(
-      begin: 0.0,
-      end: 0.43,
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Interval(
-          0.4, 0.8,
-          curve: Curves.easeOut,
-        )
-      )
-    );
-
   }
 
   void toggle() async {
@@ -159,4 +165,47 @@ class _ProgressBarState extends State<ProgressBar>
   }
 }
 
+class PercentageLabel extends StatelessWidget {
+  const PercentageLabel({
+    Key key,
+    this.opacity,
+    this.height,
+    this.percent,
+    this.offset,
+  }) : super(key: key);
 
+  final double opacity;
+  final double height;
+  final String percent;
+  final Offset offset;
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Opacity(
+        opacity: opacity,
+        child: Transform.translate(
+          offset: Offset(0, -40) + offset,
+          child: Center(
+            child: Container(
+              height: height * 4 + 20,
+              width: 100,
+              color: Colors.white,
+              child: Center(
+                child: Text("$percent%",
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodyText1
+                        .copyWith(
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.w400,
+                        )),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
