484ee24ffdbfcff55762a2061a998c5064bf02b1
Denzell Ford <fdenzell@gmail.com>
2020-12-25 02:04:59 -0600

    almost finished long walks

---
+++ longwalks/lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+var backgroundColor = Color(0xFFF7F6F2);
+var blueAccent = Color(0xFF1d2d3f);
+var grayAccent = Color(0xFF94928e);
--- longwalks/lib/main.dart
+++ longwalks/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:longwalks/constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +14,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: backgroundColor,
+        iconTheme: IconThemeData(
+          color: blueAccent,
+        ),
       ),
       home: MyHomePage(),
     );
@@ -26,26 +31,8 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class ProfilePicture extends StatelessWidget {
-  const ProfilePicture({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(30),
-      child: SizedBox(
-          width: 50,
-          height: 50,
-          child:
-              Image.network("https://thispersondoesnotexist.com/image")),
-    );
-  }
-}
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +40,7 @@ class _MyHomePageState extends State<MyHomePage> {
         items: [
           BottomNavigationBarItem(
             label: "Daily",
-            icon: Icon(Icons.bolt),
+            icon: Icon(Icons.wb_sunny),
           ),
           BottomNavigationBarItem(
             label: "Conversations",
@@ -61,29 +48,202 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           BottomNavigationBarItem(
             label: "Profile",
-            icon: Icon(Icons.settings_cell),
+            icon: ProfilePicture(border: false),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Icon(Icons.menu),
-            SafeArea(
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  vertical: 5,
-                  horizontal: 20,
-                ),
-                child: Row(children: [
-                  ProfilePicture(),
-                ]),
-              ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TopBar(),
+              Weekdays(),
+              DailyQuote(),
+              SizedBox(height: 30),
+              PersonCard(),
+              SizedBox(height: 10),
+              PersonCard(),
+              SizedBox(height: 10),
+              PersonCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Weekdays extends StatelessWidget {
+  const Weekdays({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      Weekday(day: "Sun"),
+    ]);
+  }
+}
+
+class Weekday extends StatelessWidget {
+  const Weekday({
+    Key key,
+    String day,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class ProfilePicture extends StatelessWidget {
+  final bool border;
+
+  const ProfilePicture({
+    Key key,
+    this.border = true,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        if (border) 
+            CircleAvatar(
+              radius: 30,
+              backgroundColor: Colors.white,
             ),
-            Icon(Icons.notifications),
+
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: blueAccent,
+          backgroundImage: NetworkImage("https://thispersondoesnotexist.com/image")),
+      ],
+    );
+  }
+}
+
+class PersonCard extends StatelessWidget {
+  const PersonCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+
+    return Container(
+      width: double.infinity,
+      height: size.height * 0.53,
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30),
+        color: Colors.white,
+      ),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 15),
+          Row(children: <Widget>[
+            ProfilePicture(border: true),
+            SizedBox(width: 10),
+            Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Alex",
+                    style: Theme.of(context).textTheme.bodyText1.copyWith(
+                        color: blueAccent,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text("Now")
+                ]),
+            Spacer(),
+            Icon(Icons.more_horiz),
           ]),
+          Text.rich(TextSpan(
+              text:
+                  "My focus on this week is to ",
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  color: blueAccent, fontSize: 20, fontWeight: FontWeight.w400),
+              children: <InlineSpan>[
+                TextSpan(
+                  text: "enjoy what nature has to offer",
+                  style: Theme.of(context).textTheme.bodyText1.copyWith(
+                      color: blueAccent,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+              ])),
+          SizedBox(height: 20),
+          ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(
+                  "https://images.unsplash.com/photo-1533760881669-80db4d7b4c15?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzF8fGJlYWNofGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")),
+          SizedBox(height: 20),
+          Row(
+            children: <Widget>[
+              Icon(Icons.message_outlined),
+              Spacer(),
+              Icon(Icons.share),
+            ]
+          ),
         ],
       ),
+    );
+  }
+}
+
+class DailyQuote extends StatelessWidget {
+  const DailyQuote({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: <Widget>[
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Presence",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline4
+                  .copyWith(color: blueAccent, fontWeight: FontWeight.w500)),
+          Text("Every Monday, find focus",
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyText1
+                  .copyWith(color: grayAccent, fontSize: 20))
+        ],
+      ),
+    ]);
+  }
+}
+
+class TopBar extends StatelessWidget {
+  const TopBar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Row(children: [
+        Icon(Icons.menu),
+        Spacer(),
+        Container(
+          child: Row(children: [
+            ProfilePicture(),
+          ]),
+        ),
+        Spacer(),
+        Icon(Icons.notifications_outlined),
+      ]),
     );
   }
 }
