a5dce2e5696ac8a556f0aeb87fd75bc6ea82352d
Denzell Ford <fdenzell@gmail.com>
2021-01-17 11:50:30 -0600

    adjusted a few things

--- PartyPage/src/components/BackgroundBlur.svelte
+++ PartyPage/src/components/BackgroundBlur.svelte
@@ -1,9 +1,9 @@
 <style lang="scss">
     div {
       background: rgba(255, 255, 255, 0.2);
-      backdrop-filter: blur(20px);
-      height: 100vh;
+      backdrop-filter: blur(5px);
       width: 100%;
+      height: 100%;
     }
 </style>
 
